d6a832f0b1e9d019fc0cbd8503485835d0ff6abd
YosiLeibman <yosil.770@gmail.com>
2018-06-20 09:48:06 +0300

    upload fully works
    sometimes it return undefine if folder name - may help make new var with this

--- routes/uploads.js
+++ routes/uploads.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const http = require('http');
+const formidable = require('formidable');
 
+var folder;
 // Donate page
 router.get('/', function(req, res){
       res.render('uploadchooser', {
@@ -8,8 +11,22 @@ router.get('/', function(req, res){
 });
 
 router.get('/:folder', function(req, res){
+    folder = req.params.folder;
     res.render('uploads', {
-        folder: req.params.folder
+        folder: folder
+    });
+});
+
+// Save uploads file
+router.post('/uploads', function(req, res, next){
+    var it = this;
+    var form = new formidable.IncomingForm();
+    form.parse(req);
+    form.on('fileBegin', function (name, file){
+        file.path = __dirname + '/../public/downloads/' + folder + '/' + file.name;
+    });
+    form.on('file', function (name, file){
+        console.log('Uploaded ' + file.name);
     });
 });
 module.exports = router;
