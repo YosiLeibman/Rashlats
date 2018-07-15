6343f259a8a7c9f6fa33747472fd9f110df1410f
YosiLeibman <yosil.770@gmail.com>
2018-07-15 16:17:08 +0300

    change  path to linux and youtube tries

--- routes/downloads.js
+++ routes/downloads.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
+const path = require('path');
 
 // page
 router.get('/', function(req, res){
-    hachyalfiles = fs.readdirSync(__dirname + "/../public/downloads/hachayal");
-    heoressfiles = fs.readdirSync(__dirname + "/../public/downloads/heoress");
+    hachyalfiles = fs.readdirSync(path.join(__dirname, "/../","public/downloads/hachayal"));
+    heoressfiles = fs.readdirSync(path.join(__dirname, "/../","public/downloads/heoress"));
     
     res.render('downloads', {
         hachyalfiles: hachyalfiles,
